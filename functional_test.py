5bd90128b74796d2bf779275eca08cb9b56edecf
liscju <piotr.listkiewicz@gmail.com>
2015-05-31 17:59:20 +0200

    Before refactoring

--- functional_test.py
+++ functional_test.py
@@ -36,6 +36,12 @@ class NewVisitorTest(unittest.TestCase):
         # into a text box
         inputbox.send_keys('Buy peacock feathers')
 
+        # When she hits enter, the page updates
+        # and now the page lists
+        # "1: Buy peacock feathers" as an item
+        # in to-do list
+        inputbox.send_keys(Keys.ENTER)
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
@@ -43,19 +49,24 @@ class NewVisitorTest(unittest.TestCase):
             "New to-do item did not appear in table"
         )
 
-        # When she hits enter, the page updates
-        # and now the page lists
-        # "1: Buy peacock feathers" as an item
-        # in to-do list
-        inputbox.send_keys(Keys.ENTER)
-
         # There is still a text box inviting her
         # to add another item
         # She enters "Use peacock feathers to make a fly"
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '2. Use peacock feathers to make a fly' for row in rows),
+            "2. Use peacock.... did not appear in table"
+        )
 
         # The page updates again,and now shows both
         # items on her list
+
+        self.fail('Finish the test!')
 
         # Edith wonders wheteher the site will
         # remember her list, Then she sees
