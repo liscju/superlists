e987b60af5d117efd3da58a47f3328277f3fc51e
liscju <piotr.listkiewicz@gmail.com>
2015-06-23 14:15:53 +0200

    Created new app accounts for managing user accounts,moved common front-end libraries to static directory in superlist directory

--- superlists/settings.py
+++ superlists/settings.py
@@ -103,6 +103,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR,'../static') )
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'superlists', 'static'),
+)
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
