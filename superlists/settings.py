9acef9f2bec9d01116ca6fb2ad86e3dbc154c8d9
liscju <piotr.listkiewicz@gmail.com>
2015-06-23 13:57:56 +0200

    Added logging all errors on console

--- superlists/settings.py
+++ superlists/settings.py
@@ -108,3 +108,20 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
