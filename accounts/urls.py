32ba53d51c1d5c8ff3b30d6ee1cf9764d255be06
liscju <piotr.listkiewicz@gmail.com>
2015-06-24 16:16:21 +0200

    Added logging off functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ __author__ = 'lee'
 
 urlpatterns = patterns(
     '',
-    url(r'^login$', 'accounts.views.persona_login',name='persona_login')
+    url(r'^login$', 'accounts.views.persona_login',name='persona_login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout')
 )
