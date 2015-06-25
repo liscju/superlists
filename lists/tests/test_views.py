9e539bcd9022910154a98d3341d89718008e2eeb
liscju <piotr.listkiewicz@gmail.com>
2015-06-25 17:01:19 +0200

    Url, placeholder view, and first cut templates for my_lists:Before Moving Down to the next layer:what view passes to the template

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -157,6 +157,47 @@ class NewListTest(TestCase):
 class NewItemTest(TestCase):
     pass
 
+class MyListsTest(TestCase):
+
+    def test_my_lists_url_renders_my_lists_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
