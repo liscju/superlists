abd1c44f45d10c22edbb8e5fb5933d58cb96bdca
liscju <piotr.listkiewicz@gmail.com>
2015-06-26 11:09:32 +0200

    New_list view tries to assign owner but cant

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,4 +1,5 @@
 from unittest import skip
+from django.contrib.auth import get_user_model
 from django.http.request import HttpRequest
 from django.template.loader import render_to_string
 
@@ -6,7 +7,7 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from django.utils.html import escape
 from lists.forms import ItemForm, EMPTY_LIST_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm
-from lists.views import home_page, view_list
+from lists.views import home_page, view_list, new_list
 from lists.models import Item, List
 
 class HomePageTest(TestCase):
@@ -157,11 +158,27 @@ class NewListTest(TestCase):
 class NewItemTest(TestCase):
     pass
 
+User = get_user_model()
 class MyListsTest(TestCase):
 
     def test_my_lists_url_renders_my_lists_template(self):
+        User.objects.create(email='a@b.com')
         response = self.client.get('/lists/users/a@b.com/')
         self.assertTemplateUsed(response, 'my_lists.html')
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
+
+    def test_list_owner_is_saved_if_user_is_authenticated(self):
+        request = HttpRequest()
+        request.user = User.objects.create(email='a@b.com')
+        request.POST['text'] = 'new list item'
+        new_list(request)
+        list_ = List.objects.first()
+        self.assertEqual(list_.owner, request.user)
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
@@ -6,6 +7,7 @@ from django.shortcuts import render, redirect
 from lists.forms import ItemForm, ExistingListItemForm
 from lists.models import Item, List
 
+User = get_user_model()
 
 def home_page(request):
     return render(request,'home.html', {'form': ItemForm()})
@@ -24,14 +26,17 @@ def view_list(request,list_id):
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
-        list_ = List.objects.create()
+        list_ = List()
+        list_.owner = request.user
+        list_.save()
         form.save(for_list=list_)
         return redirect(list_)
 
     return render(request, 'home.html', {'form': form})
 
 def my_lists(request, email):
-    return render(request, 'my_lists.html')
+    owner = User.objects.get(email=email)
+    return render(request, 'my_lists.html', {'owner': owner})
 
 
 
